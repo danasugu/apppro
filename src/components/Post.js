85363bd09860c33c999a1316aa0904f3d9c4bf76
Dana Sugu <dana.sugu@gmail.com>
2021-06-19 15:47:28 +0300

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
 class Post extends Component {
+	state = {
+		name: 'Mario',
+		age: '25',
+		skills: [ 'React', 'Javascript', 'HTML', 'Laravel' ]
+	};
 	render() {
 		return (
 			<div>
 				<h1>this is post component</h1>
 				{new Date().toLocaleString()}
+				<p>Name: {this.state.name}</p>
+				<p>Age: {this.state.age}</p>
+				<p>Skills: {this.state.skills.join(', ')}</p>
 			</div>
 		);
 	}
