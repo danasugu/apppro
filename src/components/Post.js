a815f7a1cc016a015427a4897378a81d37abc02a
Dana Sugu <dana.sugu@gmail.com>
2021-06-20 19:13:01 +0300

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,11 @@
+import { getDefaultNormalizer } from '@testing-library/react';
 import React, { Component } from 'react';
 
 class Post extends Component {
 	state = {
 		name   : 'Mario',
+		email  : 'mario@gmail.com',
+		phone  : '',
 		age    : '25',
 		skills : [
 			'React',
@@ -36,6 +39,19 @@ class Post extends Component {
 		});
 	};
 
+	handleChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('name', this.state.name);
+		console.log('email', this.state.email);
+		console.log('phone', this.state.phone);
+	};
+
 	render() {
 		return (
 			<div>
@@ -48,10 +64,11 @@ class Post extends Component {
 				<button onMouseOver={this.handleMouseOver}>Mouse Over</button>
 				<p onCopy={this.handleCopy}>This is a test paragraph</p>
 				<button onClick={this.handleChangeState}>Change state</button>
-				<form>
-					<input type='text' name='name' />
-					<input type='email' name='email' />
-					<input type="phone" name="phone" />
+				<form onSubmit={this.handleSubmit}>
+					<input type='text' name='name' onChange={this.handleChange} />
+					<input type='email' name='email' onChange={this.handleChange} />
+					<input type='phone' name='phone' onChange={this.handleChange} />
+					<button type='submit'>Submit</button>
 				</form>
 			</div>
 		);
