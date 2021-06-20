90f85c03ebda461e67b59a5e7f816180a56e97f2
Dana Sugu <dana.sugu@gmail.com>
2021-06-20 19:29:07 +0300

    Update Student.js

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,1 +1,17 @@
-import React {Component} from 'react';
+import { Component } from 'react';
+// import React {Component} from 'react';
+
+class Student extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Student Details</h1>
+				<p>Name: DS</p>
+				<p>Email: ds@ds.com</p>
+				<p>Phone: 0987654321</p>
+			</div>
+		);
+	}
+}
+
+export default Student;
