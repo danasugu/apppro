d24401e076bb57927953a002198ea3b535519a08
Dana Sugu <dana.sugu@gmail.com>
2021-06-21 19:10:59 +0300

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -74,7 +74,7 @@ class Post extends Component {
 					<button type='submit'>Submit</button>
 				</form>
 				<br />
-				<Student />
+				<Student name="Allen" email="allen@allen.com" phone="97614286321"   />
 			</div>
 		);
 	}
