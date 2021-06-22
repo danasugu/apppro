235135fc35433bce362bb163d2436ab45531457c
Dana Sugu <dana.sugu@gmail.com>
2021-06-22 20:01:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 
 import Post from './components/Post';
+import Teacher from './components/Teacher';
 
 function App() {
 	return (
 		<div className='App'>
 			<Post />
+			<Teacher name="Peter" email="e@e.com" phone="098769887000" />
 		</div>
 	);
 }
