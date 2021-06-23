3f6e33054c1eed864f20834c4a64dc2abc14c04d
Dana Sugu <dana.sugu@gmail.com>
2021-06-23 18:50:52 +0300

    Create Employee.js

---
+++ src/components/Employee.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
+
+class Employee extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Employee</h1>
+      </div>
+    )
+  }
+}
+
+export default Employee;
