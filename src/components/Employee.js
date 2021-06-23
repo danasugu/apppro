3a881c3da7812c140f2e91041b74bacee67ef390
Dana Sugu <dana.sugu@gmail.com>
2021-06-23 19:16:20 +0300

    Update Employee.js

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -3,6 +3,16 @@ import React, {Component} from 'react';
 
 class Employee extends Component{
   render(){
+    const employees = this.props.employees;
+    const employeeList = employees.map(employee=>{
+        return(
+          <div>
+            <p>Name: {employee.name}</p>
+            <p>Email: {employee.email}</p>
+            <p>Phone: {employee.phone}</p>
+          </div>
+        )
+    })
     return(
       <div>
         <h1>Employee</h1>
