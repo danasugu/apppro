bc2cece71502e54926894d920639ea2cb7e5ee72
Dana Sugu <dana.sugu@gmail.com>
2021-06-24 19:49:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@
 // import { render } from '@testing-library/react';
 import React, {Component} from 'react';
 import './App.css';
+import './E'
 
 // import Post from './components/Post';
 // import Teacher from './components/Teacher';
 import Employee from './components/Employee';
 
 
-class App() extends Component {
-	state = {
-		employee:[
-			{id:1, name:'DS', email:'ds@ds.com', phone:'0987654321'},
-			{id:2, name:'ASDDS', email:'asads@ds.com', phone:'12327654321'},
-			{id:3, name:'GERDVC', email:'dsferdf@ds.com', phone:'7165231321'}
-		]
-	}
+class Employeep() extends Component {
+
 	render(){
 		return (
 		<div className='App'>
